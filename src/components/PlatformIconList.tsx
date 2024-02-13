ccea1818bd6c988a52f6871ec418f98239932716
hemantaad <hemantaad@gmail.com>
2024-02-12 22:33:30 -0800

    Added platform icons and critic score in the gamecard component.

--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -1,16 +1,41 @@
-import { Text } from "@chakra-ui/react";
+import { HStack, Icon, Text } from "@chakra-ui/react";
+import {
+  FaWindows,
+  FaXbox,
+  FaPlaystation,
+  FaApple,
+  FaLinux,
+  FaAndroid,
+} from "react-icons/fa";
+
+import { MdPhoneIphone } from "react-icons/md";
+import { SiNintendo } from "react-icons/si";
+import { BsGlobe } from "react-icons/bs";
+import { IconType } from "react-icons";
 import { Platform } from "../hooks/useGames";
 
 interface Props {
   platforms: Platform[];
 }
 const PlatformIconList = ({ platforms }: Props) => {
+  const iconMap: { [key: string]: IconType } = {
+    pc: FaWindows,
+    playstation: FaPlaystation,
+    xbox: FaXbox,
+    mac: FaApple,
+    android: FaAndroid,
+    nintendo: SiNintendo,
+    linux: FaLinux,
+    ios: MdPhoneIphone,
+    web: BsGlobe,
+  };
+
   return (
-    <>
+    <HStack marginY={1}>
       {platforms.map((platform) => (
-        <Text>{platform.name}</Text>
+        <Icon as={iconMap[platform.slug]} color="gray.500" />
       ))}
-    </>
+    </HStack>
   );
 };
 
